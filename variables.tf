8db783c1456fe2402f9680a8929376576502ae4c
Kameswara Rao Pudi <22082423+kameshpudi@users.noreply.github.com>
2022-12-19 21:37:22 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,23 @@
 
+variable "location" {
+  description = "Azure location where the resources are deployed to"
+  type        = string
+  default     = "westeurope"
+}
+
+variable "rg_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "d-emea-app-rg"
+}
+
+variable "plan_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "kkreqsres2022_plan"
+}
+variable "service_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "kkreqsres2022"
+}
