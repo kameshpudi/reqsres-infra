b930ddb04f7e756353ce0d8015b69fb92d23622b
Kameswara Rao Pudi <22082423+kameshpudi@users.noreply.github.com>
2022-12-19 21:33:56 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,28 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+  tags     = var.tags
+}
 
+resource "azurerm_app_service_plan" "plan" {
+  name                = var.plan_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = var.service_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.plan.id
+
+  site_config {
+    linuxFxVersion = "PYTHON|3.11"
+  }
+
+}
