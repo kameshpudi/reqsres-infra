9ab0dccbd76cb0d22e6ec097d1503cfe79c41b16
Kameswara Rao Pudi <22082423+kameshpudi@users.noreply.github.com>
2022-12-19 21:18:41 +0100

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,14 @@
+remote_state {
+ backend = "azurerm"
+ generate = {
+   path      = "backend.tf"
+   if_exists = "overwrite_terragrunt"
+ }
+ config = {
+   resource_group_name  = "d-emea-iac-tfstate-rg" # $REPLACE: Adapt to your resource group defined in _init
+   storage_account_name = "demeatfstate2022"              # $REPLACE: Adapt to your storage account defined in _init
+   container_name       = "tfstate"
+   key                  = "${path_relative_to_include()}/terraform.tfstate"
+   access_key           = get_env("EMEA_ARM_ACCESS_KEY")
+ }
+}
