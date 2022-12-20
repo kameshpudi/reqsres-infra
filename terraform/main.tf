199ed52be45c858f039e3aa78d30587080bd0882
Kameswara Rao Pudi <22082423+kameshpudi@users.noreply.github.com>
2022-12-20 08:10:43 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,14 +15,30 @@ resource "azurerm_app_service_plan" "plan" {
   }
 }
 
-resource "azurerm_app_service" "service" {
-  name                = var.service_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.plan.id
+
+resource "azurerm_storage_account" "stg" {
+  name                     = var.stg_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_function_app" "function" {
+  name                       = var.service_name
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_app_service_plan.plan.id
+  storage_account_name       = azurerm_storage_account.stg.name
+  storage_account_access_key = azurerm_storage_account.stg.primary_access_key
+  os_type                    = "linux"
+  version                    = "~4"
+
+  app_settings {
+    FUNCTIONS_WORKER_RUNTIME = "python"
+  }
 
   site_config {
     linux_fx_version = "python|3.11"
   }
-
 }
